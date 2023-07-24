36282952fc82d252be932dd434aeaddf9e613232
Roselle Ebarle <me@roselleebarle.com>
2023-07-24 11:40:42 +0800

    Update tracker code for callsine

--- uqcdi.js
+++ uqcdi.js
@@ -1,5 +1,6 @@
 const LR_USER_IDENTIFIER = "lr_id";
-const LR_EVENT_API = "https://api.callsine.com/v1/analytics/create/";
+const LR_EVENT_API = "https://api.leadresolution.io/api/analytics/events/";
+const CALLSINE_EVENT_API = "https://api.callsine.com/v1/analytics/create/";
 
 function setCookie(cname, cvalue, exdays) {
   const d = new Date();
@@ -55,5 +56,72 @@ function lr_sendEvent() {
   xhr.send(data);
 }
 
+function sendToLREventAPI(data, callback) {
+  var xhrPost = new XMLHttpRequest();
+  xhrPost.open("POST", LR_EVENT_API, true);
+  xhrPost.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+  xhrPost.onreadystatechange = function () {
+    if (xhrPost.readyState === 4) {
+      if (xhrPost.status === 200) {
+        console.log("Data sent successfully to LR_EVENT_API");
+        callback(data);
+      } else {
+        console.error("Error sending data to LR_EVENT_API");
+      }
+    }
+  };
+  xhrPost.send(JSON.stringify(data));
+}
+
+function sendToCallsineEventAPI(data) {
+  data.reference_id = "email_id";
+  var xhrPost = new XMLHttpRequest();
+  xhrPost.open("POST", CALLSINE_EVENT_API, true);
+  xhrPost.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+  xhrPost.onreadystatechange = function () {
+    if (xhrPost.readyState === 4) {
+      if (xhrPost.status === 200) {
+        console.log("Data sent successfully to CALLSINE_EVENT_API");
+      } else {
+        console.error("Error sending data to CALLSINE_EVENT_API");
+      }
+    }
+  };
+  xhrPost.send(JSON.stringify(data));
+}
+
+function lr_sendEvent_V2() {
+  // Send a GET request to retrieve IP details
+  var xhrIP = new XMLHttpRequest();
+  xhrIP.open("GET", "https://ipapi.co/json/", true);
+
+  xhrIP.onreadystatechange = function () {
+    if (xhrIP.readyState === 4 && xhrIP.status === 200) {
+      var response = JSON.parse(xhrIP.responseText);
+
+      let user_id = getCookie(LR_USER_IDENTIFIER); // Assuming getCookie is a function you have
+      var data = {
+        timestamp: new Date().toISOString(),
+        user_id: user_id,
+        event: "page_view",
+        reference_id: "person_id",
+        page: window.location.href,
+        country_code: response.country_code,
+        country_name: response.country_name,
+        ip_v4: response.ip,
+        user_agent: navigator.userAgent,
+      };
+
+      sendToLREventAPI(data, sendToCallsineEventAPI);
+    } else if (xhrIP.readyState === 4) {
+      console.error("Error fetching IP details");
+    }
+  };
+
+  xhrIP.send();
+}
+
 lr_init();
-lr_sendEvent();
+lr_sendEvent_V2();
